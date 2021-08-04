99248d78d39c1e235072f97ad9695b80019eb63e
ismail harmanda <iharmanda@hotmail.com>
2021-08-04 17:13:49 +0300

    use useEffect() to storage login info and call it for just 1 time.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,8 +10,13 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", 1);
     setIsLoggedIn(true);
   };
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    storedUserLoggedInInformation === "1" && setIsLoggedIn(true);
+  }, []);
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
